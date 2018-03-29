b60bb1a9fa28258a999eb793ee3086b8573b2205
rfw118 <36180359+rfw118@users.noreply.github.com>
2018-03-28 21:03:33 -0400

    Delete IEncryptStuff.java

--- src/JeremyStrategyChallenge1/IEncryptStuff.java
+++
@@ -1,8 +0,0 @@
-package JeremyStrategyChallenge1;
-
-public interface IEncryptStuff {
-	
-	public abstract String EncryptFile();
-	public abstract String DecryptFile();
-	public abstract String KeyPair(String sPrivate, String sPublic);
-}
